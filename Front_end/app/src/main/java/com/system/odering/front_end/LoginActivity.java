fd877c6a38a250e2d53a69af92cabd427a184dd2
Jason Mckay <214022560@mycput.ac.za>
2016-09-10 15:17:03 +0200

    This is the front-end application, with a login and register. *** Please update BEFORE commit ***.

---
+++ Front_end/app/src/main/java/com/system/odering/front_end/LoginActivity.java
@@ -0,0 +1,117 @@
+package com.system.odering.front_end;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.system.odering.front_end.model.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText txt_login_email;
+    private EditText txt_login_password;
+    private String email = null;
+    private String password = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txt_login_email = (EditText)findViewById(R.id.txt_login_email);
+        txt_login_password = (EditText)findViewById(R.id.txt_login_password);
+
+    }
+
+    public void actionLoginLogin(View view) {
+        if(txt_login_email.getText().equals("") || txt_login_password.getText().equals("")){
+            Toast.makeText(this, "A name and password are required.", Toast.LENGTH_LONG).show();
+        }else{
+            email = txt_login_email.getText().toString();
+            password = txt_login_password.getText().toString();
+            int response = validateCredentials();
+            if(response == 1){
+                //Intent intent = new Intent(this, ...class);
+                //startActivity(intent);
+            }else if(response == 2){
+                Toast.makeText(this, "Incorrect password for this email.", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this, "User doesn't exist, please register.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void actionLoginRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    /*
+    * Method - validateCredentials(User)
+    * This method is used to validate user login details.
+    * This method will determine if the user is allowed
+    * access to the application. If user exists the method
+    * will return 1, if user exists but password is incorrect
+    * the method will return 2, if user does not exist will
+    * return 3. */
+    public int validateCredentials(){
+        int response = 0;
+        try {
+            response = new HttpRequestTask().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Integer>{
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            try{
+                final String url = "http://0.0.0.0:8080/api/users/verify/{email}/{password}";
+                RestTemplate rest = new RestTemplate();
+                rest.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                HashMap<String, String> map = new HashMap<String, String>(2);
+                map.put("email", email);
+                map.put("password", password);
+                ResponseEntity<User> userResponseEntity = rest.postForEntity(url, null, User.class, map);
+
+                // if condition will determine the response
+                // based on the http response type (NOT_FOUND, OK, ...)
+                /*if(userResponseEntity.getStatusCode() == HttpStatus.OK){
+                    return 1;
+                }*/
+
+            }catch(HttpClientErrorException loginError){
+
+                /*if(loginError.getStatusCode() == HttpStatus.NOT_ACCEPTABLE)
+                    return 2;
+                }else if(loginError.getStatusCode() == HttpStatus.NOT_FOUND){
+                    return 3;
+                }*/
+
+                System.out.println("ERROR: SEAND/RECEIVE_REQUEST - " + loginError);
+            }catch(Exception e){
+                System.out.println("ERROR: OTHER - " + e);
+            }
+            //if other exception
+            return 1;
+        }
+    }
+}
---
+++ Front_end/app/src/main/java/com/system/odering/front_end/RegisterActivity.java
@@ -0,0 +1,102 @@
+package com.system.odering.front_end;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.system.odering.front_end.model.User;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText txt_name_register;
+    private EditText txt_email_register;
+    private EditText txt_password_register;
+    private EditText txt_confirm_password_register;
+    private String name = null;
+    private String email = null;
+    private String password = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        txt_name_register = (EditText)findViewById(R.id.txt_register_name);
+        txt_email_register = (EditText)findViewById(R.id.txt_register_email);
+        txt_password_register = (EditText)findViewById(R.id.txt_register_password);
+        txt_confirm_password_register = (EditText)findViewById(R.id.txt_register_confirm_password);
+    }
+
+    public void actiionRegister(View view) {
+        if(txt_name_register.getText().equals("") || txt_email_register.getText().equals("") || txt_password_register.getText().equals("") || txt_confirm_password_register.getText().equals("")){
+            Toast.makeText(this, "All fields are required to be filled in.", Toast.LENGTH_LONG).show();
+        }else{
+            name = txt_name_register.getText().toString();
+            email = txt_email_register.getText().toString();
+            password = txt_password_register.getText().toString();
+            if(validateEmail(email)) {
+                if (validatePassword(password)) {
+                    if (password.equals(txt_confirm_password_register.getText())) {
+                        new HttpRequestTask().execute();
+                    } else {
+                        Toast.makeText(this, "Password must match.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Invalid password entered.", Toast.LENGTH_LONG).show();
+                }
+            }else{
+                Toast.makeText(this, "Invalid email address entered.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private boolean validateEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    public Boolean validatePassword(String password){
+        String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, User[]> {
+        User[] user = new User[0];
+        @Override
+        protected User[] doInBackground(Void... params) {
+            try{
+                final String url = "http://0.0.0.0:8080/api/users";
+                RestTemplate rest = new RestTemplate();
+                rest.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                HttpEntity<User> request = new HttpEntity<>(new User(name, email, password));
+                user = rest.postForObject(url, request, User[].class);
+            }catch(HttpClientErrorException registerError) {
+                System.out.println("ERROR: SEAND/RECEIVE_REQUEST - " + registerError);
+            }catch(Exception e){
+                System.out.println("ERROR: OTHER - " + e);
+            }
+            return user;
+        }
+
+        @Override
+        protected void onPostExecute(User[] users) {
+            if(users != null) {
+                //Intent intent = new Intent(RegisterActivity.this.getApplicationContext(), ...class);
+                //startActivity(intent);
+            }
+        }
+    }
+}

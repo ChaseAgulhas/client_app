83a6954e9f8a50c04223bde78b377a047a742d17
Chad February <chadfebruary@gmail.com>
2016-12-14 00:11:54 +0200

    Added a repo and service for the Menu

---
+++ Front_end/app/src/main/java/com/system/odering/front_end/domain/order/IMenu.java
@@ -0,0 +1,10 @@
+package com.system.odering.front_end.domain.order;
+
+/**
+ * Created by cfebruary on 2016/12/13.
+ */
+public interface IMenu {
+    Long getId();
+    Category getCategory();
+    FoodItem getFoodItem();
+}
--- Front_end/app/src/main/java/com/system/odering/front_end/domain/order/Menu.java
+++ Front_end/app/src/main/java/com/system/odering/front_end/domain/order/Menu.java
@@ -7,31 +7,64 @@ import java.util.Map;
 /**
  * Created by cfebruary on 2016/09/25.
  */
-public class Menu implements Serializable{
-    private String category;
+public class Menu implements Serializable, IMenu{
+    private Long id;
+    private Category category;
+    private FoodItem foodItem;
 
     private Menu(Builder builder)
     {
+        this.id = builder.id;
         this.category = builder.category;
+        this.foodItem = builder.foodItem;
     }
 
-    public String getCategory() {
+    public Long getId(){return id;}
+
+    public Category getCategory() {
         return category;
     }
 
+    public FoodItem getFoodItem(){return foodItem;}
+
     public String toString()
     {
-        return "Category: " + getCategory();
+        return "Category: " + getCategory() + "\nFood item: " + getFoodItem();
     }
 
     public static Map<String, Object> ITEM_MAP = new HashMap<String, Object>();
 
-    private static class Builder
+    public static class Builder
     {
-        private String category;
+        private Long id;
+        private Category category;
+        private FoodItem foodItem;
 
         public Builder(){}
 
+        public Builder id(Long id){
+            this.id = id;
+            return this;
+        }
 
+        public Builder category(Category category){
+            this.category = category;
+            return this;
+        }
+
+        public Builder foodItem(FoodItem foodItem){
+            this.foodItem = foodItem;
+            return this;
+        }
+
+        public Builder copy(Long id, Category category, FoodItem foodItem){
+            this.id = id;
+            this.category = category;
+            this.foodItem = foodItem;
+
+            return this;
+        }
+
+        public Menu build(){return new Menu(this);}
     }
 }
